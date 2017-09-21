d21669dcb677b21246fdfc832fad91217326fd48
sporlando <sporlando91@gmail.com>
2017-09-21 15:40:19 -0400

    Add output functions and change record keys
    - Output and display functions were added to data.clj. These still need to be integrated with the cli-options in core.clj
    - Keys in the record maps were changed from Strings to Keywords. These were originally kept as Strings since it is best practices to have kabob-cased keywords (and these were camel case). Upon further deliberation, the effectiveness of having the keys as keywords instead of Strings made this a worthwhile change

--- src/gr_person_hw/data.clj
+++ src/gr_person_hw/data.clj
@@ -1,7 +1,38 @@
 (ns gr-person-hw.data
   (:require [gr-person-hw.config :as config :only [datafile-directory]]
+            [clojure.pprint :refer [pprint print-table]]
             [clojure.string :as str]
             [clj-time.format :as tf]))
+
+;;; Utility
+
+(defn date-str->date-obj
+  "Transforms a date string into a date object with a given format."
+  [date-str format]
+  (tf/parse (tf/formatter format) date-str))
+
+(defn date-obj->date-str
+  "Transforms a date object into a date string with a given format."
+  [date-obj format]
+  (tf/unparse (tf/formatter format) date-obj))
+
+(defn transform-dates
+  "Either replaces all date objects from a collection of maps with date strings
+   OR replaces all date strings from a collection with date objects.
+   The first item of the :date-field is checked and that determines the
+   transformation."
+  [coll date-field format]
+  (let [date-fn (if (string? (date-field (first coll)))
+                  (partial date-str->date-obj) (partial date-obj->date-str))]
+    (mapv #(assoc % date-field
+                    (date-fn (date-field %) format)) coll)))
+
+(defn print-pretty
+  "Uses print-table to display the given data."
+  [data]
+  (print-table data))
+
+;;; Extraction
 
 (defn delim-str->maps
   "Extract a delimited string (e.g. slurped from a file) into a vector of maps
@@ -9,12 +40,9 @@
    into date objects."
   [data delim-re]
   (let [data (map #(str/split % delim-re) (str/split-lines data))
-        header (first data)]
-    ;; Converting to date objects will help with comparisons later on. These
-    ;; objects will get (tf/unparse) to display properly.
-    (mapv #(assoc % "DateOfBirth" (tf/parse (tf/formatter config/date-format)
-                                            (get % "DateOfBirth")))
-          (map #(zipmap header %) (rest data)))))
+        header (map keyword (first data))]
+    (transform-dates
+     (map #(zipmap header %) (rest data)) :DateOfBirth config/date-format)))
 
 (defn extract-person-file
   "Loads a person file from the filesystem and returns the parsed data."
@@ -30,3 +58,37 @@
   (into [] (apply concat
                   (for [file filenames]
                     (extract-person-file file)))))
+
+;;; Output
+
+(defn sort-gender-lastname
+  "Sorts the data by :Gender (female before male), then by :LastName ascending."
+  [data]
+  (sort-by (juxt :Gender :LastName) data))
+
+(defn sort-dateofbirth
+  "Sorts the data by :DateOfBirth ascending."
+  [data]
+  (sort-by :DateOfBirth data))
+
+(defn sort-lastname-descending
+  "Sorts the data by :LastName descending."
+  [data]
+  (sort-by :LastName #(compare %2 %1) data))
+
+(defn output-all
+  "Displays all defined outputs."
+  [data]
+  (let [output1 {:title "By :Gender then :LastName ascending"
+                 :output (sort-gender-lastname data)}
+        output2 {:title "By :DateOfBirth"
+                 :output (sort-dateofbirth data)}
+        output3 {:title "By :LastName descending"
+                 :output (sort-lastname-descending data)}
+        all-outputs [output1 output2 output3]]
+    (for [output all-outputs]
+      (do
+        (println \newline)
+        (pprint (:title output))
+        (print-pretty
+         (transform-dates (:output output) :DateOfBirth config/date-format))))))
--- test/gr_person_hw/data_test.clj
+++ test/gr_person_hw/data_test.clj
@@ -4,6 +4,36 @@
             [clj-time.core :as time :only [date-time]]
             [clojure.test :refer [deftest is testing]]))
 
+;;; Utility Tests
+
+(deftest test-date-str->date-obj
+  (testing "Transforming a date string into a date object."
+    (is (= (data/date-str->date-obj "12/31/1991" "M/d/YYYY")
+           (time/date-time 1991 12 31)))
+    (is (= (data/date-str->date-obj "3/4/2010" "M/dd/YYYY")
+           (time/date-time 2010 3 4)))))
+
+(deftest test-date-obj->date-str
+  (testing "Transforming a date object into a date string with a format."
+    (is (= (data/date-obj->date-str (time/date-time 1991 12 31) "M/d/YYYY")
+           "12/31/1991"))
+    (is (= (data/date-obj->date-str (time/date-time 2010 3 4) "M/d/YYYY")
+           "3/4/2010"))))
+
+(deftest test-transform-dates
+  (testing "Replace all date strings with date objects or vice versa."
+    (is (= (data/transform-dates
+            [{:date "5/4/1997"}{:date "12/5/2010"}] :date "M/d/YYYY")
+           [{:date (time/date-time 1997 5 4)}
+            {:date (time/date-time 2010 12 5)}]))
+    (is (= (data/transform-dates
+            [{:date (time/date-time 1997 5 4)}
+             {:date (time/date-time 2010 12 5)}] :date "M/d/YYYY")
+           [{:date "5/4/1997"}
+            {:date "12/5/2010"}]))))
+
+;;; Extraction Tests
+
 (def comma-delim-string
   "LastName,FirstName,Gender,FavoriteColor,DateOfBirth\r\nWest,Jerry,Male,Blue,11/17/1991\r\nJean,Billie,Female,Red,9/4/1934")
 
@@ -17,59 +47,59 @@
   (testing "Testing parsing accuracy for the three types of delimiters
             as well as proper building of the final data structure."
     (is (= (data/delim-str->maps comma-delim-string #",")
-           [{"LastName" "West"
-             "FirstName" "Jerry"
-             "Gender" "Male"
-             "FavoriteColor" "Blue"
-             "DateOfBirth" (time/date-time 1991 11 17)}
-            {"LastName" "Jean"
-             "FirstName" "Billie"
-             "Gender" "Female"
-             "FavoriteColor" "Red"
-             "DateOfBirth" (time/date-time 1934 9 4)}]))
+           [{:LastName "West"
+             :FirstName "Jerry"
+             :Gender "Male"
+             :FavoriteColor "Blue"
+             :DateOfBirth (time/date-time 1991 11 17)}
+            {:LastName "Jean"
+             :FirstName "Billie"
+             :Gender "Female"
+             :FavoriteColor "Red"
+             :DateOfBirth (time/date-time 1934 9 4)}]))
     (is (= (data/delim-str->maps pipe-delim-string #"\|")
-           [{"LastName" "Johnson"
-             "FirstName" "Chad"
-             "Gender" "Male"
-             "FavoriteColor" "Blue"
-             "DateOfBirth" (time/date-time 1991 12 31)}
-            {"LastName" "Rooney"
-             "FirstName" "Mara"
-             "Gender" "Female"
-             "FavoriteColor" "Red"
-             "DateOfBirth" (time/date-time 1989 9 4)}]))
+           [{:LastName "Johnson"
+             :FirstName "Chad"
+             :Gender "Male"
+             :FavoriteColor "Blue"
+             :DateOfBirth (time/date-time 1991 12 31)}
+            {:LastName "Rooney"
+             :FirstName "Mara"
+             :Gender "Female"
+             :FavoriteColor "Red"
+             :DateOfBirth (time/date-time 1989 9 4)}]))
     (is (= (data/delim-str->maps space-delim-string #"\s+")
-           [{"LastName" "Orlando"
-             "FirstName" "Sam"
-             "Gender" "Male"
-             "FavoriteColor" "Blue"
-             "DateOfBirth" (time/date-time 1991 12 31)}
-            {"LastName" "Jones"
-             "FirstName" "Amanda"
-             "Gender" "Female"
-             "FavoriteColor" "Red"
-             "DateOfBirth" (time/date-time 1989 9 4)}]))))
+           [{:LastName "Orlando"
+             :FirstName "Sam"
+             :Gender "Male"
+             :FavoriteColor "Blue"
+             :DateOfBirth (time/date-time 1991 12 31)}
+            {:LastName "Jones"
+             :FirstName "Amanda"
+             :Gender "Female"
+             :FavoriteColor "Red"
+             :DateOfBirth (time/date-time 1989 9 4)}]))))
 
 (deftest test-extract-person-file
   (with-redefs [config/file-codec {"test_person_comma.csv" #","}
                 config/person-file-directory "./resources/ingest-files-test/"]
     (testing "Testing correctly finding a file and slurping it."
       (is (= (data/extract-person-file "test_person_comma.csv")
-             [{"LastName" "Gordon"
-               "FirstName" "Jeff"
-               "Gender" "Male"
-               "FavoriteColor" "Red"
-               "DateOfBirth" (time/date-time 1969 6 14)}
-              {"LastName" "Lively"
-               "FirstName" "Blake"
-               "Gender" "Female"
-               "FavoriteColor" "Green"
-               "DateOfBirth" (time/date-time 1977 8 27)}
-              {"LastName" "Stark"
-               "FirstName" "Arya"
-               "Gender" "Female"
-               "FavoriteColor" "Yellow"
-               "DateOfBirth" (time/date-time 1974 7 22)}])))))
+             [{:LastName "Gordon"
+               :FirstName "Jeff"
+               :Gender "Male"
+               :FavoriteColor "Red"
+               :DateOfBirth (time/date-time 1969 6 14)}
+              {:LastName "Lively"
+               :FirstName "Blake"
+               :Gender "Female"
+               :FavoriteColor "Green"
+               :DateOfBirth (time/date-time 1977 8 27)}
+              {:LastName "Stark"
+               :FirstName "Arya"
+               :Gender "Female"
+               :FavoriteColor "Yellow"
+               :DateOfBirth (time/date-time 1974 7 22)}])))))
 
 (deftest test-extract-all-files
   (with-redefs [config/file-codec {"test_person_comma.csv" #","
@@ -81,50 +111,82 @@
                                       "test_person_pipe.csv"
                                       "test_person_space.csv"])
              [;; comma.csv
-              {"LastName" "Gordon"
-               "FirstName" "Jeff"
-               "Gender" "Male"
-               "FavoriteColor" "Red"
-               "DateOfBirth" (time/date-time 1969 6 14)}
-              {"LastName" "Lively"
-               "FirstName" "Blake"
-               "Gender" "Female"
-               "FavoriteColor" "Green"
-               "DateOfBirth" (time/date-time 1977 8 27)}
-              {"LastName" "Stark"
-               "FirstName" "Arya"
-               "Gender" "Female"
-               "FavoriteColor" "Yellow"
-               "DateOfBirth" (time/date-time 1974 7 22)}
+              {:LastName "Gordon"
+               :FirstName "Jeff"
+               :Gender "Male"
+               :FavoriteColor "Red"
+               :DateOfBirth (time/date-time 1969 6 14)}
+              {:LastName "Lively"
+               :FirstName "Blake"
+               :Gender "Female"
+               :FavoriteColor "Green"
+               :DateOfBirth (time/date-time 1977 8 27)}
+              {:LastName "Stark"
+               :FirstName "Arya"
+               :Gender "Female"
+               :FavoriteColor "Yellow"
+               :DateOfBirth (time/date-time 1974 7 22)}
               ;; pipe.csv
-              {"LastName" "Flacco"
-               "FirstName" "Joe"
-               "Gender" "Male"
-               "FavoriteColor" "Red"
-               "DateOfBirth" (time/date-time 1969 6 14)}
-              {"LastName" "Petro"
-               "FirstName" "Laura"
-               "Gender" "Female"
-               "FavoriteColor" "Green"
-               "DateOfBirth" (time/date-time 1977 8 27)}
-              {"LastName" "Adams"
-               "FirstName" "Amy"
-               "Gender" "Female"
-               "FavoriteColor" "Green"
-               "DateOfBirth" (time/date-time 1974 7 22)}
+              {:LastName "Flacco"
+               :FirstName "Joe"
+               :Gender "Male"
+               :FavoriteColor "Red"
+               :DateOfBirth (time/date-time 1969 6 14)}
+              {:LastName "Petro"
+               :FirstName "Laura"
+               :Gender "Female"
+               :FavoriteColor "Green"
+               :DateOfBirth (time/date-time 1977 8 27)}
+              {:LastName "Adams"
+               :FirstName "Amy"
+               :Gender "Female"
+               :FavoriteColor "Green"
+               :DateOfBirth (time/date-time 1974 7 22)}
               ;; space.csv
-              {"LastName" "Allen"
-               "FirstName" "Tim"
-               "Gender" "Male"
-               "FavoriteColor" "Red"
-               "DateOfBirth" (time/date-time 1969 6 14)}
-              {"LastName" "Williams"
-               "FirstName" "Serena"
-               "Gender" "Female"
-               "FavoriteColor" "Green"
-               "DateOfBirth" (time/date-time 1977 8 27)}
-              {"LastName" "Williams"
-               "FirstName" "Venus"
-               "Gender" "Female"
-               "FavoriteColor" "Green"
-               "DateOfBirth" (time/date-time 1974 7 22)}])))))
+              {:LastName "Allen"
+               :FirstName "Tim"
+               :Gender "Male"
+               :FavoriteColor "Red"
+               :DateOfBirth (time/date-time 1969 6 14)}
+              {:LastName "Williams"
+               :FirstName "Serena"
+               :Gender "Female"
+               :FavoriteColor "Green"
+               :DateOfBirth (time/date-time 1977 8 27)}
+              {:LastName "Williams"
+               :FirstName "Venus"
+               :Gender "Female"
+               :FavoriteColor "Green"
+               :DateOfBirth (time/date-time 1974 7 22)}])))))
+
+;;; Output Tests
+
+(deftest test-sort-gender-lastname
+  (testing "Sort by :Gender then :LastName."
+    (is (= (data/sort-gender-lastname
+            [{:Gender "Male" :LastName "Orlando"}
+             {:Gender "Female" :LastName "Jones"}
+             {:Gender "Male" :LastName "Arty"}])
+           (seq [{:Gender "Female" :LastName "Jones"}
+                 {:Gender "Male" :LastName "Arty"}
+                 {:Gender "Male" :LastName "Orlando"}])))))
+
+(deftest test-sort-dateofbirth
+  (testing "Sort by :DateOfBirth."
+    (is (= (data/sort-dateofbirth
+            [{:DateOfBirth (time/date-time 1974 7 22) :LastName "Orlando"}
+             {:DateOfBirth (time/date-time 2012 4 7) :LastName "Jones"}
+             {:DateOfBirth (time/date-time 1974 6 10) :LastName "Arty"}])
+           (seq [{:DateOfBirth (time/date-time 1974 6 10) :LastName "Arty"}
+                 {:DateOfBirth (time/date-time 1974 7 22) :LastName "Orlando"}
+                 {:DateOfBirth (time/date-time 2012 4 7) :LastName "Jones"}])))))
+
+(deftest test-sort-lastname-descending
+  (testing "Sort by :LastName descending."
+    (is (= (data/sort-lastname-descending
+            [{:LastName "Orlando" :FirstName "Sam"}
+             {:LastName "Archibald" :FirstName "Francis"}
+             {:LastName "Zion" :FirstName "Jerry"}])
+           (seq [{:LastName "Zion" :FirstName "Jerry"}
+                 {:LastName "Orlando" :FirstName "Sam"}
+                 {:LastName "Archibald" :FirstName "Francis"}])))))
